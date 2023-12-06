f1dad989de60c6112e6452d46fdb41abf8c37e44
elijahaken <elijahaken73@gmail.com>
2023-12-06 13:38:49 -0700

    hi

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d provosmentalhealth.us-east-2.elasticbeanstalk.com --nginx --agree-tos --email cstokes1@byu.edu
+sudo certbot -n -d group14ftw.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email cstokes1@byu.edu
