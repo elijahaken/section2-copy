ca10dccf2be60586dca050cc01b6bc029389636e
taitertot18 <lilysharontait@icloud.com>
2023-12-07 09:21:40 -0700

    domain name update

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d provosmentalhealth.us-east-2.elasticbeanstalk.com  --nginx --agree-tos --email cstokes1@byu.edu
+sudo certbot -n -d provo.us-east-2.elasticbeanstalk.com  --nginx --agree-tos --email lst28@byu.edu
